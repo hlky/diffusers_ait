1f24fcced00acf9e9d10dc75ddd5b069aa789eee
hlky <hlky@hlky.ac>
2024-06-15 12:43:28 +0100

    embeddings

--- tests/embeddings.py
+++ tests/embeddings.py
@@ -360,6 +360,75 @@ class EmbeddingsTestCase(unittest.TestCase):
             msg=lambda msg: f"{msg}\n\npt ({y_pt.shape}):\n{y_pt}\n\nait ({y.shape}):\n{y}\n\n",
         )
 
+    # PixArtAlphaTextProjection
+    def _test_pixart_alpha_text_projection(
+        self,
+        shape: List[int],
+        caption_channels: int,
+        hidden_size: int,
+        dtype: str = "float16",
+        tolerance: float = 1e-5,
+    ):
+        x = get_random_torch_tensor(shape, dtype=dtype)
+        x_ait = x.clone().to(x.device, x.dtype)
+
+        op = (
+            embeddings_torch.PixArtAlphaTextProjection(
+                in_features=caption_channels,
+                hidden_size=hidden_size,
+            )
+            .eval()
+            .to(x.device, x.dtype)
+        )
+
+        state_dict_pt = cast(dict[str, torch.Tensor], op.state_dict())
+        state_dict_ait = {}
+        for key, value in state_dict_pt.items():
+            key_ait = key.replace(".", "_")
+            if value.ndim == 4 and "weight" in key:
+                value = value.permute(0, 2, 3, 1).contiguous()
+            value = value.to(x.device, x.dtype)
+            state_dict_ait[key_ait] = value
+
+        with torch.inference_mode():
+            y_pt: torch.Tensor = op.forward(x)
+        y = torch.empty_like(y_pt).to(x.device, x.dtype)
+
+        X = Tensor(
+            shape=shape,
+            dtype=dtype,
+            name="X",
+            is_input=True,
+        )
+
+        op = embeddings.PixArtAlphaTextProjection(
+            in_features=caption_channels,
+            hidden_size=hidden_size,
+            dtype=dtype,
+        )
+        op.name_parameter_tensor()
+        Y = op.forward(X)
+        Y = mark_output(Y, "Y")
+
+        target = detect_target()
+        test_name = f"test_pixart_alpha_text_projection_{dtype}_c{caption_channels}_dim{hidden_size}"
+        inputs = {"X": x_ait}
+        module = compile_model(
+            Y,
+            target,
+            "./tmp",
+            test_name,
+            constants=state_dict_ait,
+        )
+        module.run_with_tensors(inputs, [y])
+        torch.testing.assert_close(
+            y,
+            y_pt.to(y.dtype),
+            rtol=tolerance,
+            atol=tolerance,
+            msg=lambda msg: f"{msg}\n\npt ({y_pt.shape}):\n{y_pt}\n\nait ({y.shape}):\n{y}\n\n",
+        )
+
     def test_timestep_embedding(self):
         self._test_timestep_embedding([1, 320], in_channels=320, tolerance=1e-3)
 
@@ -428,6 +497,22 @@ class EmbeddingsTestCase(unittest.TestCase):
             layer_norm=False,
             bias=True,
             tolerance=2e-3,
+            dtype="float16",
+        )
+
+    def test_pixart_alpha_text_projection(self):
+        self._test_pixart_alpha_text_projection(
+            shape=[1, 13, 4096],
+            caption_channels=4096,
+            hidden_size=1152,
+            tolerance=5e-4,
+            dtype="float16",
+        )
+        self._test_pixart_alpha_text_projection(
+            shape=[1, 42, 4096],
+            caption_channels=4096,
+            hidden_size=1152,
+            tolerance=5e-4,
             dtype="float16",
         )
 
