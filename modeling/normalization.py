8953240567a5be3adc1f6ec516d619b71a0bd92a
hlky <hlky@hlky.ac>
2024-06-16 10:28:02 +0100

    normalization

--- modeling/normalization.py
+++ modeling/normalization.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional, Tuple, Union, Iterable
+from typing import Any, Dict, Iterable, Optional, Tuple, Union
 
 from aitemplate.compiler import ops
 
@@ -11,13 +11,16 @@ from .embeddings import (
     PixArtAlphaCombinedTimestepSizeEmbeddings,
 )
 
+
 def get_shape(x):
     shape = [it.value() for it in x._attrs["shape"]]
     return shape
 
 
 class RMSNorm(nn.Module):
-    def __init__(self, dim, eps: float, elementwise_affine: bool = True, dtype: str = "float16"):
+    def __init__(
+        self, dim, eps: float, elementwise_affine: bool = True, dtype: str = "float16"
+    ):
         super().__init__()
 
         self.eps = eps
@@ -191,3 +194,58 @@ class AdaGroupNorm(nn.Module):
         x = ops.group_norm(self.num_groups, channels.symbolic_value())
         x = x * (1 + scale) + shift
         return x
+
+
+class AdaLayerNormContinuous(nn.Module):
+    def __init__(
+        self,
+        embedding_dim: int,
+        conditioning_embedding_dim: int,
+        # NOTE: It is a bit weird that the norm layer can be configured to have scale and shift parameters
+        # because the output is immediately scaled and shifted by the projected conditioning embeddings.
+        # Note that AdaLayerNorm does not let the norm layer have scale and shift parameters.
+        # However, this is how it was implemented in the original code, and it's rather likely you should
+        # set `elementwise_affine` to False.
+        elementwise_affine: bool = True,
+        eps: float = 1e-5,
+        bias: bool = True,
+        norm_type: str = "layer_norm",
+        dtype: str = "float16",
+    ):
+        super().__init__()
+        self.silu = ops.silu
+        self.linear = nn.Linear(
+            conditioning_embedding_dim, embedding_dim * 2, bias=bias, dtype=dtype
+        )
+        if norm_type == "layer_norm":
+            self.norm = nn.LayerNorm(
+                embedding_dim,
+                eps=eps,
+                elementwise_affine=elementwise_affine,
+                bias=bias,
+                dtype=dtype,
+            )
+        elif norm_type == "rms_norm":
+            self.norm = RMSNorm(embedding_dim, eps, elementwise_affine, dtype=dtype)
+        else:
+            raise ValueError(f"unknown norm_type {norm_type}")
+
+    def forward(self, x: Tensor, conditioning_embedding: Tensor) -> Tensor:
+        emb = self.linear(self.silu(conditioning_embedding))
+        scale, shift = ops.chunk()(emb, 2, dim=1)
+        x = self.norm(x) * ops.unsqueeze(1)(1 + scale) + ops.unsqueeze(1)(
+            shift[:, None, :]
+        )
+        return x
+
+
+class GlobalResponseNorm(nn.Module):
+    def __init__(self, dim: int, dtype: str = "float16"):
+        super().__init__()
+        self.gamma = nn.Parameter([1, 1, 1, dim], dtype=dtype)
+        self.beta = nn.Parameter([1, 1, 1, dim], dtype=dtype)
+
+    def forward(self, x: Tensor):
+        gx = ops.vector_norm(dim=(1, 2), keepdim=True)(x)
+        nx = gx / (ops.reduce_mean(dim=-1, keepdim=True)(gx) + 1e-6)
+        return self.gamma.tensor() * (x * nx) + self.beta.tensor() + x
