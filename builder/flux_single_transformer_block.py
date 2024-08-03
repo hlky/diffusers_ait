9ceae9e5aeb277ebbe7a5f8ed7856149f6082f27
hlky <hlky@hlky.ac>
2024-08-03 05:04:36 +0100

    builder: FluxTransformerBlock, FluxSingleTransformerBlock, constants example

--- builder/flux_single_transformer_block.py
+++ builder/flux_single_transformer_block.py
@@ -19,9 +19,9 @@ ait = modeling.transformers.transformer_flux.FluxSingleTransformerBlock
 config, _, pt = load_config(config_file="builder/flux_dev_config.json")
 
 ait_module = ait(
-    dim=config['num_attention_heads'] * config['attention_head_dim'],
-    num_attention_heads=config['num_attention_heads'],
-    attention_head_dim=config['attention_head_dim'],
+    dim=config["num_attention_heads"] * config["attention_head_dim"],
+    num_attention_heads=config["num_attention_heads"],
+    attention_head_dim=config["attention_head_dim"],
 )
 ait_module.name_parameter_tensor()
 
@@ -39,31 +39,51 @@ h_w = height * width
 hidden_states = Tensor(
     [
         batch,
-        h_w+512,
-        config['num_attention_heads'] * config['attention_head_dim'],
+        h_w + 512,
+        config["num_attention_heads"] * config["attention_head_dim"],
     ],
     name="hidden_states",
     is_input=True,
 )
 temb = Tensor(
-    [batch, config['num_attention_heads'] * config['attention_head_dim']],
+    [batch, config["num_attention_heads"] * config["attention_head_dim"]],
     name="temb",
     is_input=True,
 )
 image_rotary_emb = Tensor(
-    [batch, 1, h_w+512, 64, 2, 2], # check 64
+    [batch, 1, h_w + 512, 64, 2, 2],  # check 64
     name="image_rotary_emb",
     is_input=True,
-    dtype="float32"
+    dtype="float32",
 )
 
 
 hidden_states_out = ait_module.forward(
-    hidden_states=hidden_states,
-    temb=temb,
-    image_rotary_emb=image_rotary_emb
+    hidden_states=hidden_states, temb=temb, image_rotary_emb=image_rotary_emb
 )
 hidden_states_out = mark_output(hidden_states_out, "hidden_states_out")
+
+# import torch
+# import safetensors
+# from aitemplate.testing.benchmark_ait import benchmark_module
+# from safetensors.torch import load_file
+# weights = safetensors.safe_open("G:/FLUX.1-dev/flux-dev/transformer/diffusion_pytorch_model-00002-of-00003.safetensors", "pt")
+# single_transformer_block = {}
+# for key in weights.keys():
+#     if not key.startswith("single_transformer_blocks.0."):
+#         continue
+#     single_transformer_block[key.replace("single_transformer_blocks.0.", "").replace(".", "_")] = weights.get_tensor(key).to(torch.float16)
+# save_file(single_transformer_block, "H:/single_transformer_block.0.safetensors")
+# constants = load_file("H:/single_transformer_block.0.safetensors", "cuda")
+# module = compile_model(
+# hidden_states_out,
+# target,
+# "./tmp",
+# model_name,
+# constants=constants,
+# do_constant_folding=False,
+# )
+# benchmark_module(module, count=50, repeat=4, graph_mode=True)
 
 target = detect_target()
 compile_model(
--- builder/flux_transformer_block.py
+++ builder/flux_transformer_block.py
@@ -19,9 +19,9 @@ ait = modeling.transformers.transformer_flux.FluxTransformerBlock
 config, _, pt = load_config(config_file="builder/flux_dev_config.json")
 
 ait_module = ait(
-    dim=config['num_attention_heads'] * config['attention_head_dim'],
-    num_attention_heads=config['num_attention_heads'],
-    attention_head_dim=config['attention_head_dim'],
+    dim=config["num_attention_heads"] * config["attention_head_dim"],
+    num_attention_heads=config["num_attention_heads"],
+    attention_head_dim=config["attention_head_dim"],
 )
 ait_module.name_parameter_tensor()
 
@@ -40,7 +40,7 @@ hidden_states = Tensor(
     [
         batch,
         h_w,
-        config['num_attention_heads'] * config['attention_head_dim'],
+        config["num_attention_heads"] * config["attention_head_dim"],
     ],
     name="hidden_states",
     is_input=True,
@@ -49,21 +49,21 @@ encoder_hidden_states = Tensor(
     [
         batch,
         seq_len,
-        config['num_attention_heads'] * config['attention_head_dim'],
+        config["num_attention_heads"] * config["attention_head_dim"],
     ],  # allow more tokens
     name="encoder_hidden_states",
     is_input=True,
 )
 temb = Tensor(
-    [batch, config['num_attention_heads'] * config['attention_head_dim']],
+    [batch, config["num_attention_heads"] * config["attention_head_dim"]],
     name="temb",
     is_input=True,
 )
 image_rotary_emb = Tensor(
-    [batch, 1, h_w+512, 64, 2, 2], # check 64
+    [batch, 1, h_w + 512, 64, 2, 2],  # check 64
     name="image_rotary_emb",
     is_input=True,
-    dtype="float32"
+    dtype="float32",
 )
 
 
@@ -71,13 +71,37 @@ encoder_hidden_states_out, hidden_states_out = ait_module.forward(
     hidden_states=hidden_states,
     encoder_hidden_states=encoder_hidden_states,
     temb=temb,
-    image_rotary_emb=image_rotary_emb
+    image_rotary_emb=image_rotary_emb,
 )
-encoder_hidden_states_out = mark_output(encoder_hidden_states_out, "encoder_hidden_states_out")
+encoder_hidden_states_out = mark_output(
+    encoder_hidden_states_out, "encoder_hidden_states_out"
+)
 hidden_states_out = mark_output(hidden_states_out, "hidden_states_out")
 
+# import torch
+# from aitemplate.testing.benchmark_ait import benchmark_module
+# import safetensors
+# from safetensors.torch import load_file
+# weights = safetensors.safe_open("G:/FLUX.1-dev/flux-dev/transformer/diffusion_pytorch_model-00001-of-00003.safetensors", "pt")
+# transformer_blocks = {}
+# for key in weights.keys():
+#     if not key.startswith("transformer_blocks.0."):
+#         continue
+#     transformer_blocks[key.replace("transformer_blocks.0.", "").replace(".", "_")] = weights.get_tensor(key).to(torch.float16)
+# save_file(transformer_blocks, "H:/transformer_blocks.0.safetensors")
+# constants = load_file("H:/transformer_blocks.0.safetensors", "cuda")
+# module = compile_model(
+# hidden_states_out,
+# target,
+# "./tmp",
+# model_name,
+# constants=constants,
+# do_constant_folding=False,
+# )
+# benchmark_module(module, count=50, repeat=4, graph_mode=True)
+
 target = detect_target()
-compile_model(
+module = compile_model(
     [encoder_hidden_states_out, hidden_states_out],
     target,
     "./tmp",
