1d95cf0b59fda075fb2e987505cf4218cd8db7df
hlky <hlky@hlky.ac>
2024-08-03 17:13:11 +0100

    builder: split flux, experimental split constants

--- builder/flux_transformer_block.py
+++ builder/flux_transformer_block.py
@@ -1,111 +1,159 @@
+import os
+import safetensors
+import shutil
+import subprocess
+import torch
+
 from aitemplate.compiler import compile_model
 from aitemplate.frontend import IntVar, Tensor
 from aitemplate.testing import detect_target
 from aitemplate.utils.import_path import import_parent
+from aitemplate.utils.misc import is_windows
 
 from config import load_config, mark_output
-
 
 import_parent(filepath=__file__, level=1)
 
 import modeling
 
-batch_size = 1
-resolution = 512, 1024
-height, width = resolution, resolution
+module_extension = ".dll" if is_windows() else ".so"
+module_out_path = "H:/ait_modules/flux-dev"
+weights_path = "G:/flux-dev/transformer"
 
-model_name = "FluxTransformerBlock"
-ait = modeling.transformers.transformer_flux.FluxTransformerBlock
+constants_path = "./tmp/constants/FluxTransformerBlock"
+os.makedirs(constants_path, exist_ok=True)
+
 config, _, pt = load_config(config_file="builder/flux_dev_config.json")
-
-ait_module = ait(
-    dim=config["num_attention_heads"] * config["attention_head_dim"],
-    num_attention_heads=config["num_attention_heads"],
-    attention_head_dim=config["attention_head_dim"],
-)
-ait_module.name_parameter_tensor()
-
-output_name = "Y"
 
 vae_scale_factor = 16
 seq_len = 512
 
-batch = 1
-height = IntVar(
-    [height[0] // vae_scale_factor, height[1] // vae_scale_factor], "height"
+weights_1 = safetensors.safe_open(
+    f"{weights_path}/diffusion_pytorch_model-00001-of-00003.safetensors", "pt"
 )
-width = IntVar([width[0] // vae_scale_factor, width[1] // vae_scale_factor], "width")
-h_w = height * width
-hidden_states = Tensor(
-    [
-        batch,
-        h_w,
-        config["num_attention_heads"] * config["attention_head_dim"],
-    ],
-    name="hidden_states",
-    is_input=True,
+weights_2 = safetensors.safe_open(
+    f"{weights_path}/diffusion_pytorch_model-00002-of-00003.safetensors", "pt"
 )
-encoder_hidden_states = Tensor(
-    [
-        batch,
-        seq_len,
-        config["num_attention_heads"] * config["attention_head_dim"],
-    ],  # allow more tokens
-    name="encoder_hidden_states",
-    is_input=True,
-)
-temb = Tensor(
-    [batch, config["num_attention_heads"] * config["attention_head_dim"]],
-    name="temb",
-    is_input=True,
-)
-image_rotary_emb = Tensor(
-    [batch, 1, h_w + 512, 64, 2, 2],  # check 64
-    name="image_rotary_emb",
-    is_input=True,
-    dtype="float32",
+weights_3 = safetensors.safe_open(
+    f"{weights_path}/diffusion_pytorch_model-00003-of-00003.safetensors", "pt"
 )
 
 
-encoder_hidden_states_out, hidden_states_out = ait_module.forward(
-    hidden_states=hidden_states,
-    encoder_hidden_states=encoder_hidden_states,
-    temb=temb,
-    image_rotary_emb=image_rotary_emb,
-)
-encoder_hidden_states_out = mark_output(
-    encoder_hidden_states_out, "encoder_hidden_states_out"
-)
-hidden_states_out = mark_output(hidden_states_out, "hidden_states_out")
+def compile(constants, model_name, do_compile=False, do_build=False):
+    resolution = 768, 1024
+    height, width = resolution, resolution
 
-# import torch
-# from aitemplate.testing.benchmark_ait import benchmark_module
-# import safetensors
-# from safetensors.torch import load_file
-# weights = safetensors.safe_open("G:/FLUX.1-dev/flux-dev/transformer/diffusion_pytorch_model-00001-of-00003.safetensors", "pt")
-# transformer_blocks = {}
-# for key in weights.keys():
-#     if not key.startswith("transformer_blocks.0."):
-#         continue
-#     transformer_blocks[key.replace("transformer_blocks.0.", "").replace(".", "_")] = weights.get_tensor(key).to(torch.float16)
-# save_file(transformer_blocks, "H:/transformer_blocks.0.safetensors")
-# constants = load_file("H:/transformer_blocks.0.safetensors", "cuda")
-# module = compile_model(
-# hidden_states_out,
-# target,
-# "./tmp",
-# model_name,
-# constants=constants,
-# do_constant_folding=False,
-# )
-# benchmark_module(module, count=50, repeat=4, graph_mode=True)
+    ait = modeling.transformers.transformer_flux.FluxTransformerBlock
+    config, _, pt = load_config(config_file="builder/flux_dev_config.json")
 
-target = detect_target()
-module = compile_model(
-    [encoder_hidden_states_out, hidden_states_out],
-    target,
-    "./tmp",
-    model_name,
-    constants=None,
-    do_constant_folding=False,
-)
+    ait_module = ait(
+        dim=config["num_attention_heads"] * config["attention_head_dim"],
+        num_attention_heads=config["num_attention_heads"],
+        attention_head_dim=config["attention_head_dim"],
+    )
+    ait_module.name_parameter_tensor()
+
+    batch = 1
+    height = IntVar(
+        [height[0] // vae_scale_factor, height[1] // vae_scale_factor], "height"
+    )
+    width = IntVar(
+        [width[0] // vae_scale_factor, width[1] // vae_scale_factor], "width"
+    )
+    h_w = height * width
+    hidden_states = Tensor(
+        [
+            batch,
+            h_w,
+            config["num_attention_heads"] * config["attention_head_dim"],
+        ],
+        name="hidden_states",
+        is_input=True,
+    )
+    encoder_hidden_states = Tensor(
+        [
+            batch,
+            seq_len,
+            config["num_attention_heads"] * config["attention_head_dim"],
+        ],  # allow more tokens
+        name="encoder_hidden_states",
+        is_input=True,
+    )
+    temb = Tensor(
+        [batch, config["num_attention_heads"] * config["attention_head_dim"]],
+        name="temb",
+        is_input=True,
+    )
+    image_rotary_emb = Tensor(
+        [batch, 1, h_w + seq_len, 64, 2, 2],  # check 64
+        name="image_rotary_emb",
+        is_input=True,
+        dtype="float32",
+    )
+
+    encoder_hidden_states_out, hidden_states_out = ait_module.forward(
+        hidden_states=hidden_states,
+        encoder_hidden_states=encoder_hidden_states,
+        temb=temb,
+        image_rotary_emb=image_rotary_emb,
+    )
+    encoder_hidden_states_out = mark_output(
+        encoder_hidden_states_out, "encoder_hidden_states_out"
+    )
+    hidden_states_out = mark_output(hidden_states_out, "hidden_states_out")
+
+    target = detect_target()
+    compile_model(
+        [encoder_hidden_states_out, hidden_states_out],
+        target,
+        "./tmp",
+        model_name,
+        dll_name=model_name + module_extension,
+        constants=constants,
+        do_constant_folding=False,
+        do_compile=do_compile,
+        do_build=do_build,
+    )
+
+
+for block_idx in range(0, config["num_layers"]):
+    prefix = "transformer_blocks"
+    constants = {}
+    block_prefix = f"{prefix}.{block_idx}."
+    if block_idx != 0 and os.path.exists(f"{constants_path}/{model_name + '.bin'}"):
+        continue
+    for weights in [weights_1, weights_2, weights_3]:
+        for key in weights.keys():
+            if not key.startswith(block_prefix):
+                continue
+            constants[key.replace(block_prefix, "").replace(".", "_")] = (
+                weights.get_tensor(key).to(torch.float16)
+            )
+    model_name = f"FluxTransformerBlock.{block_idx}"
+    compile(constants, model_name, do_compile=block_idx == 0)
+    shutil.move(
+        f"./tmp/{model_name}/constants.bin",
+        f"{constants_path}/{model_name + '.bin'}",
+    )
+    if block_idx == 0:
+        shutil.move(f"./tmp/{model_name}", "./tmp/FluxTransformerBlock")
+    else:
+        shutil.rmtree(f"./tmp/{model_name}")
+
+for block_idx in range(0, config["num_layers"]):
+    model_name = f"FluxTransformerBlock.{block_idx}"
+    shutil.copy(
+        f"{constants_path}/{model_name + '.bin'}",
+        f"./tmp/FluxTransformerBlock/constants.bin",
+    )
+    if block_idx == 0:
+        cmake = 'cmake -B "tmp/FluxTransformerBlock/build" -S "tmp/FluxTransformerBlock" -G "Visual Studio 17 2022" -A x64'
+        process = subprocess.Popen(cmake)
+        process.wait()
+    command = 'msbuild "tmp/FluxTransformerBlock/build/FluxTransformerBlock.0.sln" -m /property:Configuration=Release'
+    process = subprocess.Popen(command)
+    process.wait()
+    shutil.move(
+        f"./tmp/FluxTransformerBlock/build/Release/model.dll",
+        f"{module_out_path}/{model_name + module_extension}",
+    )
