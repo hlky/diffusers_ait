42959d1bcfb3bf9ca1562f34237ed3d9def932e0
hlky <hlky@hlky.ac>
2024-08-04 10:48:56 +0100

    inference: experimental split flux with constants

--- inference/flux_dev.py
+++ inference/flux_dev.py
@@ -10,20 +10,12 @@ module_out_path = "H:/ait_modules/flux-dev"
 device = "cuda"
 
 
-def ProcessBlock(block_name, total_blocks, modules_to_load=4):
+def ProcessBlock(block_name, modules, max_blocks):
     inputs = {}
     outputs = {}
     block_idx = 0
-    loaded_idx = 0
-    pbar = tqdm.tqdm(range(0, total_blocks), desc=block_name)
-    modules: List[Model] = []
-    for idx in range(0, modules_to_load):
-        pbar.set_postfix_str(f"loading {block_name}.{idx}.dll")
-        module = Model(f"{module_out_path}/{block_name}.{idx}.dll")
-        modules.append(module)
-        loaded_idx += 1
-    while block_idx < total_blocks:
-        module = modules.pop(0)
+    pbar = tqdm.tqdm(desc=block_name, total=max_blocks)
+    for module in modules:
         if block_idx == 0:
             for name, idx in module.get_input_name_to_index_map().items():
                 shape = module.get_input_maximum_shape(idx)
@@ -41,16 +33,27 @@ def ProcessBlock(block_name, total_blocks, modules_to_load=4):
                 tensor = torch.empty(*shape, dtype=dtype).to(device)
                 outputs[name] = tensor
         module.run_with_tensors(inputs=inputs, outputs=outputs)
-        del module
-        pbar.update()
-        if loaded_idx < total_blocks:
-            pbar.set_postfix_str(f"loading {block_name}.{loaded_idx}.dll")
-            modules.append(Model(f"{module_out_path}/{block_name}.{loaded_idx}.dll"))
-            loaded_idx += 1
         block_idx += 1
+        pbar.update(1)
+    for idx in range(len(modules), max_blocks):
+        module = Model(f"{module_out_path}/{block_name}.{idx}.dll")
+        module.run_with_tensors(inputs=inputs, outputs=outputs)
+        inputs["hidden_states"] = outputs["hidden_states_out"]
+        if "encoder_hidden_states" in inputs:
+            inputs["encoder_hidden_states"] = outputs["encoder_hidden_states_out"]
+        pbar.update(1)
     return outputs
 
 
+transformer_block_modules: List[Model] = []
+for idx in range(0, 19):
+    module = Model(f"{module_out_path}/FluxTransformerBlock.{idx}.dll")
+    transformer_block_modules.append(module)
+single_transformer_block_modules: List[Model] = []
+for idx in range(0, 27):
+    module = Model(f"{module_out_path}/FluxSingleTransformerBlock.{idx}.dll")
+    single_transformer_block_modules.append(module)
+
 for _ in tqdm.tqdm(range(20), desc="steps"):
-    _ = ProcessBlock("FluxTransformerBlock", 19, modules_to_load=1)
-    _ = ProcessBlock("FluxSingleTransformerBlock", 38, modules_to_load=1)
+    _ = ProcessBlock("FluxTransformerBlock", transformer_block_modules, 19)
+    _ = ProcessBlock("FluxSingleTransformerBlock", single_transformer_block_modules, 38)
